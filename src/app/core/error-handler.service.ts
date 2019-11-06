755bdabcee58793e7064bdfbdf66ade96bf14567
Leonardo <leonardogs09192@gmail.com>
2019-11-05 21:00:19 -0300

    Corrigindo erro de RefreshToken invalido.

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,19 +1,27 @@
+import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { Injectable } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/Http';
+import { NotAuthenticatedError } from '../seguranca/mony-http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private messageService: MessageService,
+    private router: Router) { }
 
   handle(errorResponse: HttpErrorResponse) {
     let msg: string;
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+
+    } else if (errorResponse instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
 
     } else if (errorResponse.status >= 400 && errorResponse.status <= 499) {
       let error;
